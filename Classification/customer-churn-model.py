8c1b95ec5a0768900d3c0b644d7b6c8b3a6ad00a
Adeseto Akinwe <adeakinwe@gmail.com>
2026-01-03 10:44:04 +0100

    model.py

---
+++ Classification/customer-churn-model.py
@@ -0,0 +1,139 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+
+import pickle
+
+
+# DATA PREPARATION
+df = pd.read_csv('telco-customer-churn.csv')
+df.columns = df.columns.str.lower()
+
+cat_cols = list(df.dtypes[df.dtypes == 'object'].index)
+cat_cols
+
+for c in cat_cols:
+    df[c] = df[c].str.lower().str.replace(' ', '_')
+
+df.totalcharges = pd.to_numeric(df.totalcharges, errors='coerce')
+df.totalcharges = df.totalcharges.fillna(0)
+df['churn'] = (df.churn == 'yes').astype(int)
+
+num_features = ['tenure', 'monthlycharges', 'totalcharges']
+cat_features = ['gender', 'seniorcitizen', 'partner', 'dependents',
+       'phoneservice', 'multiplelines', 'internetservice',
+       'onlinesecurity', 'onlinebackup', 'deviceprotection', 'techsupport',
+       'streamingtv', 'streamingmovies', 'contract', 'paperlessbilling',
+       'paymentmethod']
+
+
+# MODEL TRAINING
+df_full_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
+df_train, df_val = train_test_split(df_full_train, test_size=0.25, random_state=42)
+y_full_train = df_full_train.churn.values
+y_test = df_test.churn.values
+
+dv = DictVectorizer(sparse=False)
+
+def train(df_train, y_train, C=1.0):
+    dicts = df_train[cat_features + num_features].to_dict(orient='records')
+
+    dv = DictVectorizer(sparse=False)
+    X_train = dv.fit_transform(dicts)
+
+    model = LogisticRegression(C=C, max_iter=3500)
+    model.fit(X_train, y_train)
+
+    return dv, model
+
+def predict(df, dv, model):
+    dicts = df[cat_features + num_features].to_dict(orient='records')
+
+    X = dv.transform(dicts)
+    y_pred = model.predict_proba(X)[:, 1]
+
+    return y_pred
+
+# PARAMS
+version = 1.0
+n_splits = 5
+C = 1.0
+
+kfold = KFold(n_splits=n_splits, shuffle=True, random_state=1)
+scores = []
+fold = 0
+
+for train_idx, val_idx in kfold.split(df_full_train):
+    df_train = df_full_train.iloc[train_idx]
+    df_val = df_full_train.iloc[val_idx]
+
+    y_train = df_train.churn.values
+    y_val = df_val.churn.values
+
+    dv, model = train(df_train, y_train, C=C)
+    y_pred = predict(df_val, dv, model)
+
+    auc = roc_auc_score(y_val, y_pred)
+    scores.append(auc)
+    print(f'fold={fold} C={C} auc={auc}')
+    fold = fold + 1
+
+print('validation results:')
+print('C=%s %.3f +- %.3f' % (C, np.mean(scores), np.std(scores)))
+
+
+# EVALUATION
+dv, model = train(df_full_train, y_full_train, C=1.0)
+y_pred = predict(df_test, dv, model)
+
+auc = roc_auc_score(y_test, y_pred)
+auc
+
+
+# MODEL SAVING AND LOADING
+output_file = f'model_{version}.bin'
+with open(output_file, 'wb') as f_out:
+    pickle.dump((dv,model), f_out)
+saved_model = 'model_1.0.bin'
+
+with open(saved_model, 'rb') as f_in:
+    dv,model = pickle.load(f_in)
+
+
+# PREDICTION FOR A NEW CUSTOMER
+
+customer = {
+    'gender': 'female',
+    'seniorcitizen': 1,
+    'partner': 'yes',
+    'dependents': 'no',
+    'phoneservice': 'no',
+    'multiplelines': 'no_phone_service',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'no',
+    'onlinebackup': 'yes',
+    'deviceprotection': 'no',
+    'techsupport': 'no',
+    'streamingtv': 'no',
+    'streamingmovies': 'no',
+    'contract': 'month-to-month',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'electronic_check',
+    'tenure': 6,
+    'monthlycharges': 29.85,
+    'totalcharges': 29.85
+}
+
+def predict_cust(customer, dv, model):
+    cust = dv.transform([customer])
+    prediction = model.predict_proba(cust)
+
+    return prediction[0,1]
+
+prediction = predict_cust(customer, dv, model)
+print(prediction)
